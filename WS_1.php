53e1f95fd5d54017727eda76ea8afeed7222e743
Adrian <acuevas3@hotmail.es>
2021-02-25 20:01:22 +0100

    Modificacio

--- WS_1.php
+++ WS_1.php
@@ -64,6 +64,26 @@ echo var_dump($jmperezperezJSON);
 echo "<br>";
 echo print_r($jmperezperezJSON);
 
+$jmperezperezJSON_DECODE = json_decode($jmperezperezJSON);
+
+echo "<br>";
+echo "<br>";
+echo var_dump($jmperezperezJSON_DECODE);
+echo "<br>";
+echo print_r($jmperezperezJSON_DECODE);
+
+echo "<br><br>";
+echo $jmperezperezJSON_DECODE[0]."<br><br>";
+
+$elements3 = '{"jmperezperez": true,"thelinmichael": false,"wizzler": false}';
+echo "3 elements string format<br>";
+var_dump($elements3)."<br>";
+print_r($elements3)."<br>";
+echo "<br>";
+$resul_arr_ass = json_decode($elements3, true);
+echo "jmperezperez ".$resul_arr_ass["jmperezperez"]."<br>";
+echo "thelinmichael ".$resul_arr_ass["thelinmichael"]."<br>";
+echo "wizzler ".$resul_arr_ass["wizzler"]."<br>";
+
 
 ?>
-
